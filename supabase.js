0f017440bc7f5b64afe6869153d49818b9a4d409
lideude <lideude@gmail.com>
2026-08-29 10:16:29 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,10 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://inveobojsaijgycxcerg.supabase.co/';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzIiwicmVmIjoiaW52ZW9ib2pzYWlqZ3l4Y2NlcmciLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc4NzkwODc5NiwiZXhwIjoyMTAzNDg0Nzk2fQ.IuGAuuPFudVZOOD9YPBhOulKATDEUoNx2FlAgI0P-2U';
+const SUPABASE_URL = 'https://inveobojsaijgycxcerg.supabase.co';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_iIKDTsncUlBiImAiSqABNg_n6Ur-rjx';
+
+export const supabase = createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
